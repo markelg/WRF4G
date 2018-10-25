6851b49091edbb55bd6033b481d022b82238c9c8
garciam <markel.garciad@gmail.com>
2018-10-25 15:56:21 +0200

    wrapper test is finally working. But binaries need to be repacked

--- wrf4g/tests/test_wrapper.py
+++ wrf4g/tests/test_wrapper.py
@@ -1,11 +1,15 @@
+import logging
 import mock
 import os
+import shutil
 import sys
+import tempfile
 import unittest
-from os import stat, path
-from collections import namedtuple
 from wrf4g.wrapper import PilotParams, WRF4GWrapper
+from mocked_functions import mock_rmtree, mock_exec_cmd
 
+log = logging.getLogger()
+log.setLevel("DEBUG")
 
 testargs = [
     "../../bin/wrf_wrapper.py",
@@ -19,66 +23,56 @@ testargs = [
 os.environ["GW_HOSTNAME"] = "bender"
 os.environ["GW_JOB_ID"] = "0"
 os.environ["GW_RESTARTED"] = "0"
+os.environ["GW_NP"] = "4"
 
 
-class MockStat(object):
-    st_mode = 0
-
-
-def mock_makedirs(idir):
-    print("\nRunning os.makedirs({})".format(idir))
-
-
-def mock_chmod(ifile, perm):
-    print("\nRunning os.chmod({}, {})".format(ifile, perm))
-
-
-def mock_stat(ifile):
-    print("\nRunning os_stat({})".format(ifile))
-    return MockStat()
-
-
-def mock_chdir(idir):
-    print("\nRunning os.chdir({})".format(idir))
-
-
-def mock_rmtree(idir):
-    print("\nRunning shutil.rmtree({})".format(idir))
-
-
-def mock_copy_file(orig, dest):
-    print("\nRunning copy_file with {} and {}".format(orig, dest))
-
-
-def mock_shutil_copyfile(orig, dest):
-    print("\nRunning shutil.copyfile with {} and {}".format(orig, dest))
-
-
-def mock_extract_file(dest, to_path):
-    print("\nExtracting file {} to {}".format(dest, to_path))
-
-
-def mock_exec_cmd(app_cmd):
-    print("\nExecuting command {}".format(app_cmd))
+def write_phony_rsl(tempdir):
+    rsldir = tempdir + "/WRFV3/run"
+    os.makedirs(rsldir)
+    rslfile = rsldir + "/rsl.out.0000"
+    with open(rslfile, "w") as rsl:
+        rsl.write("SUCCESS COMPLETE\n")
 
 
 @mock.patch.object(sys, 'argv', testargs)
-def get_wrf4g_wrapper():
-    params = PilotParams(json="test_experiment/realization.json")
+def get_wrf4g_wrapper(tempdir):
+    params = PilotParams(
+        json="test_experiment/realization.json",
+        root_path=tempdir
+    )
     return WRF4GWrapper(params)
 
 
+def create_tempdir(tempdir):
+    log.info("Running test in {}".format(tempdir))
+    bindir = tempdir + "/bin"
+    os.mkdir(bindir)
+    shutil.copy2("./test_experiment/namelist.input", tempdir)
+    shutil.copy2("./test_experiment/wrf4g_files/bin/preprocessor.default",
+                 bindir)
+    shutil.copy2("./test_experiment/wrf4g_files/bin/postprocessor.SFC", bindir)
+    write_phony_rsl(tempdir)
+
+
 class TestWRF4GWrapper(unittest.TestCase):
-    @mock.patch('wrf4g.wrapper.os.makedirs', side_effect=mock_makedirs)
-    @mock.patch('wrf4g.wrapper.os.chmod', side_effect=mock_chmod)
-    @mock.patch('wrf4g.wrapper.os_stat', side_effect=mock_stat)
-    @mock.patch('wrf4g.wrapper.os.chdir', side_effect=mock_chdir)
+    #@mock.patch('wrf4g.wrapper.os.makedirs', side_effect=mock_makedirs)
+    #@mock.patch('wrf4g.wrapper.os.chmod', side_effect=mock_chmod)
+    #@mock.patch('wrf4g.wrapper.os_stat', side_effect=mock_stat)
+    #@mock.patch('wrf4g.wrapper.os.chdir', side_effect=mock_chdir)
+
+    #@mock.patch('wrf4g.wrapper.shutil.copyfile',
+    #            side_effect=mock_shutil_copyfile)
+    #@mock.patch('wrf4g.wrapper.copy_file', side_effect=mock_copy_file)
+    #@mock.patch('wrf4g.wrapper.extract', side_effect=mock_extract_file)
     @mock.patch('wrf4g.wrapper.shutil.rmtree', side_effect=mock_rmtree)
-    @mock.patch('wrf4g.wrapper.shutil.copyfile',
-                side_effect=mock_shutil_copyfile)
-    @mock.patch('wrf4g.wrapper.copy_file', side_effect=mock_copy_file)
-    @mock.patch('wrf4g.wrapper.extract', side_effect=mock_extract_file)
     @mock.patch('wrf4g.wrapper.exec_cmd', side_effect=mock_exec_cmd)
+    @mock.patch('wrf4g.wrapper.fix_ptop')
+    @mock.patch('wrf4g.utils.namelist.get_latlon_dx')
     def test_launch(self, *args):
-        wrf4g_wrapper = get_wrf4g_wrapper()
-        wrf4g_wrapper.launch()
+        tempdir = tempfile.mkdtemp(prefix="wrf4g_test_", dir="/tmp")
+        create_tempdir(tempdir)
+        wrf4g_wrapper = get_wrf4g_wrapper(tempdir)
+        with self.assertRaises(SystemExit) as sysexit:
+            wrf4g_wrapper.launch()
+            # shutil.rmtree(tempdir)
+            self.assertEquals(sysexit.code, 0)
